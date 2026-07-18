ab61d255a33d343e91b75c708d194b19658385d8
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-07-17 21:49:47 -0300

    feat(ui): cria componente padrão para telas de login e cadastro

--- myapp/src/app/index.tsx
+++ myapp/src/app/index.tsx
@@ -1,36 +1,22 @@
-import { StyleSheet, View, Text, Image} from "react-native"
+
+import AuthLayout from "@/base/authLayout";
+import { StyleSheet, View, Text, Image, TextInput } from "react-native";
 
 export default function Index(){
     return(
-        <View style ={styles.container}>
-            <Image source={require("@/assets/logoGlobalSpeaker.png")} 
-            style ={styles.ilustration}/>
-            
-            
-        </View>
        
+        <AuthLayout>
+
+            <Text>Login</Text>
+
+        </AuthLayout>
     
 
     )
 }
 
 const styles = StyleSheet.create({
-    container : {
-        backgroundColor: "#6E00D6",
-        flex: 1,
-        padding: 40,
-        alignItems: "center",
-        
-    },
 
     
 
-    ilustration :{
-        width: "100%",
-        height: 240,
-        resizeMode:"contain",
-        marginTop : 70
-
-
-    }
 })
---
+++ myapp/src/base/authLayout.tsx
@@ -0,0 +1,76 @@
+import { StyleSheet,View, Image, } from "react-native";
+
+type AuthLayoutProps = {
+    children : React.ReactNode;         //esse tipo children vai referênciar os arquivos filhos ex; campos email, senha.
+};                                      //ReactNode é tipo qualquer coisa que o React consegue renderizar na tela.
+
+export default function AuthLayout({children}: AuthLayoutProps){
+    return(
+        <View style ={styles.mainContainer}>
+            <View style ={ styles.topContainer}>
+                <Image source={require("@/assets/logoGlobalSpeaker.png")} style ={styles.ilustration}>
+                </Image>
+            </View>
+
+            <View style={ styles.content}>
+                {children}   
+            </View>
+
+        </View>
+    );
+}
+
+        
+
+//=========================== layout base da pagina de login e cadastrar ===========================================
+
+
+
+
+const styles = StyleSheet.create({
+
+
+
+    mainContainer :{        // container principal
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#6E00D6",
+        justifyContent: "space-between",
+        
+    },
+
+
+    ilustration :{
+        width: "100%",              // imagem da logo
+        height: 200,
+        resizeMode:"contain",
+        
+        
+
+
+    },
+
+
+    topContainer :{
+       width: "100%",
+       flex: 0.35,
+       justifyContent: "center",        
+     
+       
+        
+
+    },
+
+    content :{
+        backgroundColor: "#F2F1F1",
+        width: "100%",
+        flex: 0.65,
+        borderTopRightRadius : 38,
+        borderTopLeftRadius: 38,
+        alignItems: "center",
+        padding:20,
+
+
+    },
+
+});

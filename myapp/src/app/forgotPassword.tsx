b8b1f84a00c74fd6c941bdf7c31d86d33cae8e4e
Maria Eduarda Moraes Vieira <mariaeduardamoraesvieira71@gmail.com>
2026-07-26 23:32:09 -0300

    feat(forgotpassword): cria página de redefinição de senha

---
+++ myapp/src/app/forgotPassword.tsx
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import AuthLayout from "@/components/authLayout";
+
+export default function ForgotPassword(){
+    return(
+        <AuthLayout>
+             <Text>Forgot PassWord</Text>
+        </AuthLayout>
+    )
+
+
+}
